6d3b70bcc3f08965e3591acc512c5a85b1c0ce20
Griz394 <grizzlymine394@gmail.com>
2025-07-22 22:00:50 +0400

    Initial commit

---
+++ Source/DataBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MySql.Data.MySqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neuron
+{
+    class DataBase
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost; port= 3306; username=root; password=root; database = NeuronDB");
+
+        public void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ Source/NeuronLogin.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Neuron
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    public class Functions()
+    {
+        private void Send()
+        {
+
+        }
+        private void Check()
+        {
+
+        }
+    }
+}
