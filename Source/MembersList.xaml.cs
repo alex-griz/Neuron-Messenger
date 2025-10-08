58da850b099297e4a7e734d526096bc47638d93c
Griz394 <grizzlymine394@gmail.com>
2025-10-08 22:22:32 +0400

    Список участников
    Добавлен список участников группы в окне управления участниками

--- Source/MembersList.xaml.cs
+++ Source/MembersList.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,15 +18,36 @@ namespace Neuron
 {
     public partial class MembersList : Window
     {
+        DataBase db = new DataBase();
         public MembersList()
         {
             InitializeComponent();
+            LoadContacts();
         }
         private void AddMember(object sender, RoutedEventArgs e)
         {
             AddMember addMember = new AddMember();
             addMember.Show();
-            
+        }
+        public void LoadContacts()
+        {
+            DataTable ContactsList = new DataTable();
+            using (var connection = db.GetNewConnection())
+            {
+                using (var command = new MySqlCommand("SELECT `Member` FROM `contactbase` WHERE `ChatID` = @CI", connection))
+                {
+                    command.Parameters.Add("@CI", MySqlDbType.VarChar).Value = NeuronMain.ChooseContact;
+                    using (var adapter = new MySqlDataAdapter(command))
+                    {
+                        connection.Open();
+                        adapter.Fill(ContactsList);
+                    }
+                }
+            }
+            for (int i = 0; i < ContactsList.Rows.Count; i++)
+            {
+                MembersListBox.Items.Add("@"+ContactsList.Rows[i][0].ToString());
+            }
         }
     }
 }
